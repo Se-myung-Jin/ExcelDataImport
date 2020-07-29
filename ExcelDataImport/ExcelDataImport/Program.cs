6385e28d98e01d19546b9845a162f277fdecbed3
Semyung Jin <tpaud94@naver.com>
2020-07-29 22:23:06 +0900

    폴더 선택 기능 추가
    Excel 파일 존재하는 디렉토리 세팅하는 기능 추가

--- ExcelDataImport/ExcelDataImport/Program.cs
+++ ExcelDataImport/ExcelDataImport/Program.cs
@@ -3,13 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
 
 namespace ExcelDataImport
 {
     class Program
     {
-        static void Main(string[] args)
+        [STAThread]
+        static int Main(string[] args)
         {
+            String dirPathName;
+            if (args.Length < 1)
+            {
+                FolderBrowserDialog dlg = new FolderBrowserDialog();
+
+                if (dlg.ShowDialog() == DialogResult.OK && !string.IsNullOrWhiteSpace(dlg.SelectedPath))
+                {
+                    dirPathName = dlg.SelectedPath;
+                }
+                else
+                {
+                    MessageBox.Show("파일이 존재하는 폴더를 선택하세요.");
+                    return -1;
+                }
+            }
+            else
+            {
+                dirPathName = System.IO.Path.GetFullPath(args[0]);
+            }
+
+            if (!Directory.Exists(dirPathName))
+            {
+                MessageBox.Show("존재하는 폴더를 선택해 주세요.");
+                return -1;
+            }
+
+            int retVal = 0;
+
+            return retVal;
         }
     }
 }
