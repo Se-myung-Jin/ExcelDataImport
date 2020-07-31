6aed6ab7aff0c4237fdff25cf07657bebb71fb26
Semyung Jin <tpaud94@naver.com>
2020-08-01 02:33:41 +0900

    ExecuteQuery 추가, ExcelBase에 프로퍼티 추가

--- ExcelDataImport/ExcelDataImport/ImportLib/ExcelImportBase.cs
+++ ExcelDataImport/ExcelDataImport/ImportLib/ExcelImportBase.cs
@@ -8,5 +8,8 @@ namespace ImportLib
 {
     public abstract class ExcelImportBase
     {
+        public string FileName { get; protected set; }
+        public string TableName { get; protected set; }
+        public string[] ColumnNames { get; protected set; }
     }
 }
--- ExcelDataImport/ExcelDataImport/PostgreSql.cs
+++ ExcelDataImport/ExcelDataImport/PostgreSql.cs
@@ -25,5 +25,81 @@ namespace ExcelDataImport
                 return;
             }
         }
+
+        #region ExecuteQuery
+        public bool ExecuteQuery(string query, out NpgsqlDataReader results)
+        {
+            NpgsqlCommand cmd = null;
+            results = null;
+
+            try
+            {
+                cmd = new NpgsqlCommand(query, conn);
+                results = cmd.ExecuteReader();
+            }
+            catch (PostgresException e)
+            {
+                Console.WriteLine($"query: {cmd.CommandText}");
+                Console.WriteLine($"Msg: {e.Message}");
+                Console.WriteLine($"Detail: {e.Detail}");
+                return false;
+            }
+            catch (NpgsqlOperationInProgressException e)
+            {
+                Console.WriteLine($"error : {e.Message}");
+                return false;
+            }
+            return true;
+        }
+
+        public bool ExecuteQuery(string query, ref List<Dictionary<String, object>> Rows)
+        {
+            if (ExecuteQuery(query, out var results))
+            {
+                if (results != null && results.IsClosed == false)
+                {
+                    while (results.Read())
+                    {
+                        var row = new Dictionary<String, object>();
+
+                        for (int i = 0; i < results.FieldCount; i++)
+                        {
+                            string key = results.GetName(i);
+                            object value = results[i];
+
+                            row.Add(results.GetName(i), value);
+                        }
+                        Rows.Add(row);
+                    }
+                    results.Close();
+                    return true;
+                }
+            }
+            return true;
+        }
+
+        public bool ExecuteQuery (string query)
+        {
+            var cmd = new NpgsqlCommand(query);
+            return ExecuteQuery(cmd);
+        }
+
+        public bool ExecuteQuery (NpgsqlCommand cmd)
+        {
+            try
+            {
+                cmd.Connection = conn;
+                cmd.ExecuteNonQuery();
+            }
+            catch (PostgresException e)
+            {
+                Console.WriteLine($"query: {cmd.CommandText}");
+                Console.WriteLine($"Msg: {e.Message}");
+                Console.WriteLine($"Detail: {e.Detail}");
+                return false;
+            }
+            return true;
+        }
+        #endregion
     }
 }
